af4e8df8cae546211817082d0a3354ee69e9bdbc
Kashiftariq1997 <Kashiftariq848@gmail.com>
2024-11-25 18:00:30 +0500

    Added workspace id in params

--- app/controllers/github/home_controller.rb
+++ app/controllers/github/home_controller.rb
@@ -1,9 +1,10 @@
 module Github
   class HomeController < ApplicationController
     def index
-      if params.key?(:token) && params.key?(:theme)
+      if params.key?(:token) && params.key?(:theme) && params.key?(:workspace_id)
         session[:token] = params[:token]
         session[:theme] = params[:theme]
+        session[:workspace_id] = params[:workspace_id]
         
         redirect_to home_index_path and return
       end
